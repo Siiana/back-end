7d291512a1bbf49f17eefa4ede722747bfa59a6b
Siiana <siiana-elfiyka@rambler.ru>
2019-10-22 22:43:10 +0300

    change 22.10.19

--- src/main/java/com/brainacad/HttpClientHelper.java
+++ src/main/java/com/brainacad/HttpClientHelper.java
@@ -2,9 +2,7 @@ package com.brainacad;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 
@@ -18,59 +16,70 @@ import static org.apache.http.protocol.HTTP.USER_AGENT;
 
 public class HttpClientHelper {
 
-    public static HttpResponse get(String endpointUrl, String parameters) throws IOException {
-        Map<String, String> headers=new HashMap<>();
+    private static Map<String, String> headers = new HashMap<>();
+    static {
         headers.put("Content-Type", "application/json");
-       return get (endpointUrl, parameters, headers);
-
     }
 
     //REST GET запрос
-    public static HttpResponse get(String endpointUrl, String parameters, Map<String, String> headers) throws IOException {
-        //Создаём экземпляр HTTP клиента
+     public static HttpResponse get(String endpointUrl, String parameters) throws IOException {
         HttpClient client = HttpClientBuilder.create().build();
-        //Создаём HTTP GET запрос из URL и параметров
-        HttpGet request = new HttpGet(endpointUrl+"?"+parameters);
-
-        //добавляем в запрос необходимые хедеры
-        for(String headerKey:headers.keySet()) {
+        HttpGet request = new HttpGet(endpointUrl + "?" + parameters);
+        for (String headerKey : headers.keySet()) {
             request.addHeader(headerKey, headers.get(headerKey));
         }
-
-        //выполняем запрос в HTTP клиенте и получаем ответ
         HttpResponse response = client.execute(request);
-
-        //возвращаем response
         return response;
     }
 
-
-    public static HttpResponse post(String endpointUrl, String parameters)throws IOException{
-        Map<String, String> headers=new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        return post(endpointUrl, parameters, headers);
-    }
-
-    public static HttpResponse post(String endpointUrl, String body, Map<String, String> headers) throws IOException{
-        //Создаём экземпляр HTTP клиента
+    //REST POST запрос
+    public static HttpResponse post(String endpointUrl, String body) throws IOException {
         HttpClient client = HttpClientBuilder.create().build();
-        //Создаём HTTP POST запрос из URL и параметров
         HttpPost post = new HttpPost(endpointUrl);
-
-        //добавляем в запрос необходимые хедеры
-        for(String headerKey:headers.keySet()) {
+        for (String headerKey : headers.keySet()) {
             post.addHeader(headerKey, headers.get(headerKey));
         }
-
-        //добавляем к запросу тело запроса
         post.setEntity(new StringEntity(body));
-
-        //выполняем запрос в HTTP клиенте и получаем ответ
         HttpResponse response = client.execute(post);
-
-        //возвращаем response
         return response;
     }
+
+    //REST PUT запрос
+    public static HttpResponse put(String endpointUrl, String body) throws IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPut put = new HttpPut(endpointUrl);
+        for (String headerKey : headers.keySet()) {
+            put.addHeader(headerKey, headers.get(headerKey));
+        }
+        put.setEntity(new StringEntity(body));
+        HttpResponse response = client.execute(put);
+        return response;
+    }
+
+    //REST PATCH запрос
+    public static HttpResponse patch(String endpointUrl, String body) throws IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPatch patch = new HttpPatch(endpointUrl);
+        for (String headerKey : headers.keySet()) {
+            patch.addHeader(headerKey, headers.get(headerKey));
+        }
+        patch.setEntity(new StringEntity(body));
+        HttpResponse response = client.execute(patch);
+        return response;
+    }
+
+    //REST DELETE запрос
+    public static HttpResponse delete(String endpointUrl) throws IOException {
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpDelete delete = new HttpDelete(endpointUrl);
+        for (String headerKey : headers.keySet()) {
+            delete.addHeader(headerKey, headers.get(headerKey));
+        }
+        HttpResponse response = client.execute(delete);
+        return response;
+    }
+
+
 
 
     public static String getBodyFromResponse(HttpResponse response) throws IOException {
@@ -88,32 +97,6 @@ public class HttpClientHelper {
         return result.toString();
     }
 
-    public static HttpResponse put(String endpointUrl, String parameters)throws IOException{
-        Map<String, String> headers=new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        return put(endpointUrl, parameters, headers);
-    }
-
-    public static HttpResponse put(String endpointUrl, String body, Map<String, String> headers) throws IOException{
-        //Создаём экземпляр HTTP клиента
-        HttpClient client = HttpClientBuilder.create().build();
-        //Создаём HTTP POST запрос из URL и параметров
-        HttpPut put = new HttpPut(endpointUrl);
-
-        //добавляем в запрос необходимые хедеры
-        for(String headerKey:headers.keySet()) {
-            put.addHeader(headerKey, headers.get(headerKey));
-        }
-
-        //добавляем к запросу тело запроса
-        put.setEntity(new StringEntity(body));
-
-        //выполняем запрос в HTTP клиенте и получаем ответ
-        HttpResponse response = client.execute(put);
-
-        //возвращаем response
-        return response;
-    }
 
     //TODO: допишите методы для запросов PUT, PATCH и DELETE
 }
--- src/test/java/com/brainacad/RestTest.java
+++ src/test/java/com/brainacad/RestTest.java
@@ -4,8 +4,6 @@ import org.apache.http.HttpResponse;
 import org.junit.Assert;
 import org.junit.Test;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class RestTest{
@@ -15,36 +13,22 @@ public class RestTest{
     @Test//GET метод
     public void checkGetResponseStatusCode() throws IOException {
         String endpoint="/api/users";
-
-        //Создаём переменую headers типа Map
-        //Map<String, String> headers=new HashMap<>();
-        //Добавляем в headers наш заголовок
-        //headers.put("Content-Type", "application/json");
-
-        //Выполняем REST GET запрос с нашими параметрами
-        // и сохраняем результат в переменную response.
         HttpResponse response = HttpClientHelper.get(URL+endpoint,"page=2");
-
-        //получаем статус код из ответа
+        String body=HttpClientHelper.getBodyFromResponse(response);
+        int id = JsonUtils.intFromJSONByPath(body,"$.data[0].id" );
+        int size = JsonUtils.intFromJSONByPath(body,"$.data.size()" );
+        int perPage = JsonUtils.intFromJSONByPath(body,"$.per_page" );
         int statusCode = response.getStatusLine().getStatusCode();
         System.out.println("Response Code : " + statusCode);
-        Assert.assertEquals("Response status code should be 200", 200, statusCode);
+        Assert.assertEquals("Response status code should be 200",200,statusCode);
+        Assert.assertEquals("id should be 7", 7, id);
+        Assert.assertEquals("perPage=size=6", perPage, size);
     }
 
     @Test//GET метод
     public void checkGetResponseBodyNotNull() throws IOException {
         String endpoint="/api/users";
-
-        //Создаём переменую headers типа Map
-        //Map<String, String> headers=new HashMap<>();
-        //Добавляем в headers наш заголовок
-        //headers.put("Content-Type", "application/json");
-
-        //Выполняем REST GET запрос с нашими параметрами
-        // и сохраняем результат в переменную response.
         HttpResponse response = HttpClientHelper.get(URL+endpoint,"page=2");
-
-        //Конвертируем входящий поток тела ответа в строку
         String body=HttpClientHelper.getBodyFromResponse(response);
         System.out.println(body);
         Assert.assertNotEquals("Body shouldn't be null", null, body);
@@ -53,20 +37,8 @@ public class RestTest{
     @Test//POST метод
     public void checkPostResponseStatusCode() throws IOException {
         String endpoint="/api/users";
-
-        //Создаём переменую headers типа Map
-        //Map<String, String> headers=new HashMap<>();
-        //Добавляем в headers наш заголовок
-        //headers.put("Content-Type", "application/json");
-
-        //создаём тело запроса
         String requestBody="{\"name\": \"morpheus\",\"job\": \"leader\"}";
-
-        //Выполняем REST POST запрос с нашими параметрами
-        // и сохраняем результат в переменную response.
         HttpResponse response = HttpClientHelper.post(URL+endpoint,requestBody);
-
-        //получаем статус код из ответа
         int statusCode = response.getStatusLine().getStatusCode();
         System.out.println("Response Code : " + statusCode);
         Assert.assertEquals("Response status code should be 201", 201, statusCode);
@@ -75,68 +47,60 @@ public class RestTest{
     @Test//POST метод
     public void checkPostResponseBodyNotNull() throws IOException {
         String endpoint="/api/users";
-
-        //Создаём переменую headers типа Map
-        //Map<String, String> headers=new HashMap<>();
-        //Добавляем в headers наш заголовок
-        //headers.put("Content-Type", "application/json");
-
-        //создаём тело запроса
         String requestBody="{\"name\": \"morpheus\",\"job\": \"leader\"}";
-
-        //Выполняем REST POST запрос с нашими параметрами
-        // и сохраняем результат в переменную response.
         HttpResponse response = HttpClientHelper.post(URL+endpoint,requestBody);
-
-        //Конвертируем входящий поток тела ответа в строку
         String body=HttpClientHelper.getBodyFromResponse(response);
         System.out.println(body);
         Assert.assertNotEquals("Body shouldn't be null", null, body);
     }
 
-    @Test//Put метод
+    @Test//PUT метод
     public void checkPutResponseStatusCode() throws IOException {
         String endpoint="/api/users";
-
-        //Создаём переменую headers типа Map
-        //Map<String, String> headers=new HashMap<>();
-        //Добавляем в headers наш заголовок
-        //headers.put("Content-Type", "application/json");
-
-        //создаём тело запроса
         String requestBody="{\"name\": \"morpheus\",\"job\": \"resident\"}";
-
-        //Выполняем REST POST запрос с нашими параметрами
-        // и сохраняем результат в переменную response.
         HttpResponse response = HttpClientHelper.put(URL+endpoint,requestBody);
-
-        //получаем статус код из ответа
         int statusCode = response.getStatusLine().getStatusCode();
         System.out.println("Response Code : " + statusCode);
         Assert.assertEquals("Response status code should be 200", 200, statusCode);
     }
 
-    @Test//Put метод
+    @Test//PUT метод
     public void checkPutResponseBodyNotNull() throws IOException {
         String endpoint="/api/users";
-
-        //Создаём переменую headers типа Map
-        //Map<String, String> headers=new HashMap<>();
-        //Добавляем в headers наш заголовок
-        //headers.put("Content-Type", "application/json");
-
-        //создаём тело запроса
         String requestBody="{\"name\": \"morpheus\",\"job\": \"resident\"}";
-
-        //Выполняем REST POST запрос с нашими параметрами
-        // и сохраняем результат в переменную response.
         HttpResponse response = HttpClientHelper.put(URL+endpoint,requestBody);
-
-        //Конвертируем входящий поток тела ответа в строку
         String body=HttpClientHelper.getBodyFromResponse(response);
         System.out.println(body);
         Assert.assertNotEquals("Body shouldn't be null", null, body);
+    }
 
+    @Test//PATCH метод
+    public void checkPatchResponseStatusCode() throws IOException {
+        String endpoint="/api/users";
+        String requestBody="{\"name\": \"morpheus\",\"job\": \"resident\"}";
+        HttpResponse response = HttpClientHelper.patch(URL+endpoint,requestBody);
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("Response Code : " + statusCode);
+        Assert.assertEquals("Response status code should be 200", 200, statusCode);
+    }
+
+    @Test//PATCH метод
+    public void checkPatchResponseBodyNotNull() throws IOException {
+        String endpoint="/api/users";
+        String requestBody="{\"name\": \"morpheus\",\"job\": \"resident\"}";
+        HttpResponse response = HttpClientHelper.patch(URL+endpoint,requestBody);
+        String body=HttpClientHelper.getBodyFromResponse(response);
+        System.out.println(body);
+        Assert.assertNotEquals("Body shouldn't be null", null, body);
+    }
+
+    @Test//Delete метод
+    public void checkDeleteResponseStatusCode() throws IOException {
+        String endpoint="/api/users/2";
+        HttpResponse response = HttpClientHelper.delete(URL+endpoint);
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("Response Code : " + statusCode);
+        Assert.assertEquals("Response status code should be 204", 204, statusCode);
     }
 
     //TODO: напишите по тесткейсу на каждый вариант запроса на сайте https://reqres.in
